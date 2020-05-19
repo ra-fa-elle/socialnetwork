9637fec9c3ff6d1f6409989ccdd1e58527db09ae
ra-fa-elle <raphaelle.clerici@gmail.com>
2020-05-19 20:30:22 +0200

    Ajout exo 3

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,22 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+  $preparedRequest = $PDO->prepare("SELECT * FROM user WHERE nickname=:nickname AND password=:password");
+  $preparedRequest->execute(
+    array(
+      "nickname" => $username,
+      "password" => $password
+    )
+  );
+  $users = $preparedRequest->fetchAll();
+  if (count($users) == 1) {
+    $user = $users[0];
+    return $user['id'];
+  } else {
+    return -1;
+  }
+}
--- views/DisplayPosts.php
+++ views/DisplayPosts.php
@@ -29,18 +29,30 @@
       <ul class="navbar-nav mr-auto">
       </ul>
       <ul class="navbar-nav">
-    <li class="nav-item">
-        <form class="nav-link" method="get">
+        <li class="nav-item">
+          <form class="nav-link" method="get">
             <input name="search" type="text"></input>
-        </form>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="?action=login" role="button">Login</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="?action=register" role="button">Sign Up</a>
-    </li>
-</ul>
+          </form>
+        </li>
+    <?php
+      if (isset($_SESSION['userId'])) {
+    ?>
+        <li class="nav-item">
+          <a class="nav-link" href="?action=logout" role="button">Logout</a>
+        </li>
+    <?php
+      } else {
+    ?>
+        <li class="nav-item">
+          <a class="nav-link" href="?action=login" role="button">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?action=register" role="button">Sign Up</a>
+        </li>
+    <?php
+      }
+    ?>
+      </ul>
     </div>
   </header>
 
